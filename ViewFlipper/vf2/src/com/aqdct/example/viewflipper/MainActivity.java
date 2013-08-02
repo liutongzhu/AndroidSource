819efe396c1243d60ae8f427be4e671463374306
liutongzhu <tongzhu.liu@gmail.com>
2013-08-02 19:02:00 +0800

    添加ViewFlipper2

---
+++ ViewFlipper/vf2/src/com/aqdct/example/viewflipper/MainActivity.java
@@ -0,0 +1,49 @@
+package com.aqdct.example.viewflipper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends Activity {
+
+	private ViewFlipper vf;
+	private float lastX;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		vf = (ViewFlipper) findViewById(R.id.view_flipper);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent touchevent) {
+		switch (touchevent.getAction()) {
+		case MotionEvent.ACTION_DOWN: {
+			lastX = touchevent.getX();
+			break;
+		}
+		case MotionEvent.ACTION_UP: {
+			float currentX = touchevent.getX();
+			if (lastX < currentX) {
+				if (vf.getDisplayedChild() == 0)
+					break;
+				vf.setInAnimation(this, R.anim.in_from_left);
+				vf.setOutAnimation(this, R.anim.out_to_right);
+				vf.showNext();
+			}
+			if (lastX > currentX) {
+				if (vf.getDisplayedChild() == 1)
+					break;
+				vf.setInAnimation(this, R.anim.in_from_right);
+				vf.setOutAnimation(this, R.anim.out_to_left);
+				vf.showPrevious();
+			}
+			break;
+		}
+		}
+		return false;
+	}
+}
